abfe1bfa48d76c0042e8484aef7054f94d4487f1
João Almeida <joao.rafael.almeida@ua.pt>
2022-05-04 15:38:45 +0100

    First draft for downloading random sequences

---
+++ src/config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+base_path = Path(__file__).parent
+summariesPath = (base_path / "../aux/summaries").resolve()
+dstPathOfOriginalSequences = (base_path / "../aux/original_sequences").resolve()
+locationOfDatabases = (base_path / "../aux/references").resolve()
